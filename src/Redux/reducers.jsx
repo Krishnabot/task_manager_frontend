e02f59f86b57e324add15c6e9f9b503c92c9aa0c
Krishna <libraryismyparadise@gmail.com>
2023-06-01 09:38:12 +0545

    Add reducer function for action state management

---
+++ src/Redux/reducers.jsx
@@ -0,0 +1,37 @@
+const initialState = {
+    todos: [],
+  };
+  
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return {
+          ...state,
+          todos: [...state.todos, action.payload],
+        };
+      case 'EDIT_TODO':
+        return {
+          ...state,
+          todos: state.todos.map((todo) =>
+            todo.id === action.payload.id
+              ? { ...todo, title: action.payload.title }
+              : todo
+          ),
+        };
+      case 'DELETE_TODO':
+        return {
+          ...state,
+          todos: state.todos.filter((todo) => todo.id !== action.payload),
+        };
+      case 'DELETE_COMPLETED_TODOS':
+        return {
+          ...state,
+          todos: state.todos.filter((todo) => !action.payload.includes(todo.id)),
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
+  
