62b3a6fb4c9a8ac76f8941ca9dcb7d9eed704e15
Krishna <libraryismyparadise@gmail.com>
2023-06-01 11:56:29 +0545

    Add Toggle checkbox option

--- src/Redux/reducers.jsx
+++ src/Redux/reducers.jsx
@@ -4,6 +4,13 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) => (todo.id === action.payload
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo)),
+      };
     case 'ADD_TODO':
       return {
         ...state,
