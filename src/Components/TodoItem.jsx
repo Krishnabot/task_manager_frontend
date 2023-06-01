c708d49e1d95e0184e26d4e2fcd9b989b41ecb0b
Krishna <libraryismyparadise@gmail.com>
2023-06-01 11:25:59 +0545

    Add Todo Item and Todo List

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleTodo, editTodo, deleteTodo } from '../Redux/actions';
 
-function TodoItem() {
+const TodoItem = ({ id, title, isCompleted }) => {
+  const dispatch = useDispatch();
+  const [editing, setEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+
+  const handleToggle = () => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handleEdit = () => {
+    if (editedTitle.trim() !== '') {
+      dispatch(editTodo(id, editedTitle));
+      setEditing(false);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setEditing(false);
+    setEditedTitle(title);
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(id));
+  };
+
   return (
-    <div>TodoItem</div>
-  )
-}
+    <div>
+      <input
+        type="checkbox"
+        checked={isCompleted}
+        onChange={handleToggle}
+      />
+      {editing ? (
+        <div>
+          <input
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <button onClick={handleEdit}>Save</button>
+          <button onClick={handleCancelEdit}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          <span>{title}</span>
+          <button onClick={() => setEditing(true)}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import TodoItem from './TodoItem';
+import { addTodo, deleteCompletedTodos } from '../Redux/actions';
+import { useState } from "react";
 
-function TodoList() {
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const [newTask, setNewTask] = useState('');
+
+  const handleDeleteCompleted = () => {
+    const completedTodos = todos.filter((todo) => todo.isCompleted);
+    const completedTodoIds = completedTodos.map((todo) => todo.id);
+    dispatch(deleteCompletedTodos(completedTodoIds));
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      dispatch(addTodo(newTask));
+      setNewTask('');
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) { // Enter key code
+      handleAddTask();
+    }
+  };
+
   return (
-    <div>TodoList</div>
-  )
-}
+    <div>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} {...todo} />
+      ))}
+      {todos.length > 0 && (
+        <button onClick={handleDeleteCompleted}>Delete Completed</button>
+      )}
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
